01409adbe83bbd4c3f06857ce79bdded814e65ee
赵晓鹏 <1802404723@qq.com>
2022-05-06 16:33:34 +0800

    解析时间函数完成

--- util.go
+++ util.go
@@ -6,25 +6,74 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
 
+// Datetime2Time 传入一个	接口类型的时间，返回time.Time类型
+// 只接受 string 标准格式、 int time.Time  参数
+// 如果不符合格式返回nil
+func Datetime2Time(datetime interface{}) time.Time {
+	// 返回一个time.Time
+	var input time.Time
+	//	转换
+	switch date := datetime.(type) {
+	case int:
+		input = time.Unix(int64(date), 0)
+	case string:
+		input = stringtime2time(date)
+	case int64:
+		input = time.Unix(date, 0)
+	default:
+		input = date.(time.Time)
+	}
+	return input
+}
+
+// stringtime2time 字符串格式转time.Time
+func stringtime2time(date string) time.Time {
+	var input time.Time
+	if config.Location == "" {
+		parseTime, err := time.Parse("2006-01-02 15:04:05", date)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input = parseTime
+	} else {
+		location, err := time.LoadLocation(config.Location)
+		if err != nil {
+			panic("时区加载失败")
+		}
+		parseTime, err := time.ParseInLocation("2006-01-02 15:04:05", date, location)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input = parseTime
+	}
+	return input
+}
+
 func fileExists(filePath string) (bool, error) {
+	//加载filePath文件属性
 	_, err := os.Stat(filePath)
 	if err == nil {
 		return true, nil
 	}
+	// 文件未找到
 	if errors.Is(err, os.ErrNotExist) {
 		return false, nil
 	}
-
-	return false, nil
+	// 其他错误
+	return false, err
 }
+
+// 解析json字典，为language字典
 func parseJsonIntoLang(filePath, lang string) interface{} {
+	// 读取文件内容
 	context := getContent(filePath)
 	switch lang {
 	case "ch":
 		var langch langCh
-		err := json.Unmarshal(context, &langch)
+		err := json.Unmarshal(context, &langch) // 反序列化json
 		if err != nil {
 			log.Fatalln(err)
 		}
